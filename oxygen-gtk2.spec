964cc55acd6d104c611981073656692574d5795b
Alex Baranowski <alex@euro-linux.com>
2020-01-18 17:28:54 +0100

    els-ASAP auto commit file/tag oxygen-gtk2-1.3.4-1.el7.src.rpm

---
+++ oxygen-gtk2.spec
@@ -0,0 +1,187 @@
+
+Name:    oxygen-gtk2
+Summary: Oxygen GTK+2 theme
+Version: 1.3.4
+Release: 1%{?dist}
+
+License: LGPLv2+
+Group:   User Interface/Desktops
+URL:     https://projects.kde.org/projects/playground/artwork/oxygen-gtk
+Source0: http://download.kde.org/stable/oxygen-gtk2/%{version}/src/%{name}-%{version}.tar.bz2
+
+## upstream patches
+
+BuildRequires: cmake
+BuildRequires: gtk2-devel
+
+Conflicts: oxygen-gtk < 1.2.0-2
+
+%description
+Oxygen-Gtk is a port of the default KDE widget theme (Oxygen), to gtk.
+
+It's primary goal is to ensure visual consistency between gtk-based and
+qt-based applications running under KDE. A secondary objective is to also
+have a stand-alone nice looking gtk theme that would behave well on other
+Desktop Environments.
+
+Unlike other attempts made to port the KDE oxygen theme to gtk, this
+attempt does not depend on Qt (via some Qt to Gtk conversion engine), 
+nor does render the widget appearance via hard-coded pixmaps, which 
+otherwise breaks every time some setting is changed in KDE.
+
+
+%prep
+%setup -q
+
+
+
+%build
+mkdir -p %{_target_platform}
+pushd %{_target_platform}
+%{cmake} -DOXYGEN_FORCE_KDE_ICONS_AND_FONTS=0  ..
+popd
+
+make %{?_smp_mflags} -C %{_target_platform}
+
+
+%install
+rm -rf %{buildroot}
+make install/fast DESTDIR=%{buildroot} -C %{_target_platform}
+
+
+%clean
+rm -rf %{buildroot}
+
+
+%files
+%defattr(-,root,root,-)
+%doc AUTHORS COPYING README
+%{_bindir}/oxygen-gtk-demo
+%{_libdir}/gtk-2.0/*/engines/liboxygen-gtk.so
+%{_datadir}/themes/oxygen-gtk/
+
+
+%changelog
+* Fri May 31 2013 Alexey Kurov <nucleo@fedoraproject.org> - 1.3.4-1
+- oxygen-gtk2-1.3.4
+
+* Mon Apr 22 2013 Alexey Kurov <nucleo@fedoraproject.org> - 1.3.3-1
+- oxygen-gtk2-1.3.3
+
+* Wed Feb 13 2013 Alexey Kurov <nucleo@fedoraproject.org> - 1.3.2.1-1
+- oxygen-gtk2-1.3.2.1
+
+* Fri Feb 08 2013 Rex Dieter <rdieter@fedoraproject.org> 1.3.2-2
+- Upgrade to oxygen-gtk2 1.3.2 is buggy (kde#314545)
+
+* Wed Jan 30 2013 Alexey Kurov <nucleo@fedoraproject.org> - 1.3.2-1
+- oxygen-gtk2-1.3.2
+
+* Fri Oct  5 2012 Alexey Kurov <nucleo@fedoraproject.org> - 1.3.1-1
+- oxygen-gtk2-1.3.1
+
+* Fri Jul 20 2012 Fedora Release Engineering <rel-eng@lists.fedoraproject.org> - 1.3.0-2
+- Rebuilt for https://fedoraproject.org/wiki/Fedora_18_Mass_Rebuild
+
+* Tue Jul 17 2012 Alexey Kurov <nucleo@fedoraproject.org> - 1.3.0-1
+- oxygen-gtk2-1.3.0
+
+* Fri Jun 15 2012 Alexey Kurov <nucleo@fedoraproject.org> - 1.2.5-1
+- oxygen-gtk2-1.2.5
+
+* Mon May 14 2012 Alexey Kurov <nucleo@fedoraproject.org> - 1.2.4-1
+- oxygen-gtk2-1.2.4
+
+* Sat Apr 14 2012 Alexey Kurov <nucleo@fedoraproject.org> - 1.2.3-1
+- oxygen-gtk2-1.2.3
+
+* Sat Mar 24 2012 Alexey Kurov <nucleo@fedoraproject.org> - 1.2.2.1-1
+- oxygen-gtk2-1.2.2-1
+- drop badwindow patch
+
+* Tue Mar 20 2012 Alexey Kurov <nucleo@fedoraproject.org> - 1.2.2-2
+- fix crash in claws-mail (#804790, kde#295875)
+
+* Mon Mar 12 2012 Alexey Kurov <nucleo@fedoraproject.org> - 1.2.2-1
+- oxygen-gtk2-1.2.2
+- drop BR: dbus-glib-devel
+
+* Tue Feb 28 2012 Fedora Release Engineering <rel-eng@lists.fedoraproject.org> - 1.2.1-2
+- Rebuilt for c++ ABI breakage
+
+* Thu Feb 16 2012 Alexey Kurov <nucleo@fedoraproject.org> - 1.2.1-1
+- oxygen-gtk2-1.2.1
+
+* Mon Jan 23 2012 Alexey Kurov <nucleo@fedoraproject.org> - 1.2.0-3
+- drop Obsoletes: oxygen-gtk < 1.2.0-2
+
+* Tue Jan 17 2012 Alexey Kurov <nucleo@fedoraproject.org> - 1.2.0-2
+- renamed to oxygen-gtk2
+
+* Tue Jan 17 2012 Alexey Kurov <nucleo@fedoraproject.org> - 1.2.0-1
+- oxygen-gtk2-1.2.0
+- License: LGPLv2+
+
+* Fri Jan 13 2012 Fedora Release Engineering <rel-eng@lists.fedoraproject.org> - 1.1.6-2
+- Rebuilt for https://fedoraproject.org/wiki/Fedora_17_Mass_Rebuild
+
+* Thu Dec 15 2011 Alexey Kurov <nucleo@fedoraproject.org> - 1.1.6-1
+- 1.1.6
+
+* Fri Nov 18 2011 Alexey Kurov <nucleo@fedoraproject.org> - 1.1.5-1
+- 1.1.5
+
+* Fri Oct 28 2011 Alexey Kurov <nucleo@fedoraproject.org> - 1.1.4-3
+- disable forcing KDE icons and fonts
+
+* Wed Oct 26 2011 Fedora Release Engineering <rel-eng@lists.fedoraproject.org> - 1.1.4-2
+- Rebuilt for glibc bug#747377
+
+* Sun Oct 16 2011 Alexey Kurov <nucleo@fedoraproject.org> - 1.1.4-1
+- 1.1.4
+
+* Mon Oct  3 2011 Alexey Kurov <nucleo@fedoraproject.org> - 1.1.3-3
+- fix mozilla applications detection kde#283251
+
+* Fri Sep 16 2011 Alexey Kurov <nucleo@fedoraproject.org> - 1.1.3-2
+- BR: dbus-glib-devel
+
+* Fri Sep 16 2011 Alexey Kurov <nucleo@fedoraproject.org> - 1.1.3-1
+- 1.1.3
+
+* Sat Aug 13 2011 Rex Dieter <rdieter@fedoraproject.org> 1.1.2-1
+- 1.1.2
+
+* Fri Jul 15 2011 Alexey Kurov <nucleo@fedoraproject.org> - 1.1.1-1
+- 1.1.1
+
+* Tue Jun 14 2011 Rex Dieter <rdieter@fedoraproject.org> 1.1.0-1
+- 1.1.0
+
+* Fri May 20 2011 Rex Dieter <rdieter@fedoraproject.org> 1.0.5-1
+- 1.0.5
+
+* Tue Apr 12 2011 Rex Dieter <rdieter@fedoraproject.org> 1.0.4-1
+- 1.0.4
+
+* Mon Mar 14 2011 Rex Dieter <rdieter@fedoraproject.org> 1.0.3-1
+- 1.0.3
+
+* Fri Feb 11 2011 Rex Dieter <rdieter@fedoraproject.org> 1.0.2-1
+- 1.0.2
+
+* Tue Feb 08 2011 Fedora Release Engineering <rel-eng@lists.fedoraproject.org> - 1.0.1-2
+- Rebuilt for https://fedoraproject.org/wiki/Fedora_15_Mass_Rebuild
+
+* Thu Jan 13 2011 Rex Dieter <rdieter@fedoraproject.org> 1.0.1-1
+- oxygen-gtk-1.0.1
+
+* Mon Jan 03 2011 Rex Dieter <rdieter@fedoraproject.org> - 1.0.0-2
+- drop extraneous BR: cairo-devel
+
+* Sun Dec 12 2010 Rex Dieter <rdieter@fedoraproject.org> -  1.0.0-1
+- first try
+
+
+
+
---
+++ src/src/animations/oxygenanimations.cpp
@@ -0,0 +1,356 @@
+/*
+* this file is part of the oxygen gtk engine
+* Copyright (c) 2010 Hugo Pereira Da Costa <hugo@oxygen-icons.org>
+*
+* This  library is free  software; you can  redistribute it and/or
+* modify it  under  the terms  of the  GNU Lesser  General  Public
+* License  as published  by the Free  Software  Foundation; either
+* version 2 of the License, or(at your option ) any later version.
+*
+* This library is distributed  in the hope that it will be useful,
+* but  WITHOUT ANY WARRANTY; without even  the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+* Lesser General Public License for more details.
+*
+* You should have received a copy of the GNU Lesser General Public
+* License  along  with  this library;  if not,  write to  the Free
+* Software Foundation, Inc., 51  Franklin St, Fifth Floor, Boston,
+* MA 02110-1301, USA.
+*/
+
+#include "oxygenanimations.h"
+#include "../oxygengtkutils.h"
+#include "../oxygenqtsettings.h"
+#include "../config.h"
+
+#include <cassert>
+#include <iostream>
+
+namespace Oxygen
+{
+
+    //_________________________________________
+    Animations::Animations( void ):
+        _innerShadowsEnabled( true ),
+        _hooksInitialized( false )
+    {
+        #if OXYGEN_DEBUG
+        std::cerr << "Oxygen::Animations::Animations" << std::endl;
+        #endif
+
+        // create engines
+        registerEngine( _backgroundHintEngine = new BackgroundHintEngine( this ) );
+        registerEngine( _comboEngine = new ComboEngine( this ) );
+        registerEngine( _comboBoxEngine = new ComboBoxEngine( this ) );
+        registerEngine( _comboBoxEntryEngine = new ComboBoxEntryEngine( this ) );
+        registerEngine( _dialogEngine = new DialogEngine( this ) );
+        registerEngine( _flatWidgetEngine = new FlatWidgetEngine( this ) );
+        registerEngine( _groupBoxEngine = new GroupBoxEngine( this ) );
+        registerEngine( _groupBoxLabelEngine = new GroupBoxLabelEngine( this ) );
+        registerEngine( _hoverEngine = new HoverEngine( this ) );
+        registerEngine( _mainWindowEngine = new MainWindowEngine( this ) );
+        registerEngine( _menuItemEngine = new MenuItemEngine( this ) );
+        registerEngine( _panedEngine = new PanedEngine( this ) );
+        registerEngine( _scrollBarEngine = new ScrollBarEngine( this ) );
+        registerEngine( _scrolledWindowEngine = new ScrolledWindowEngine( this ) );
+        registerEngine( _innerShadowEngine = new InnerShadowEngine( this ) );
+        registerEngine( _tabWidgetEngine = new TabWidgetEngine( this ) );
+        registerEngine( _treeViewEngine = new TreeViewEngine( this ) );
+        registerEngine( _widgetSizeEngine = new WidgetSizeEngine( this ) );
+
+        // animations specific engines
+        registerEngine( _widgetStateEngine = new WidgetStateEngine( this ) );
+        registerEngine( _arrowStateEngine = new ArrowStateEngine( this ) );
+        registerEngine( _scrollBarStateEngine = new ScrollBarStateEngine( this ) );
+        registerEngine( _tabWidgetStateEngine = new TabWidgetStateEngine( this ) );
+        registerEngine( _treeViewStateEngine = new TreeViewStateEngine( this ) );
+        registerEngine( _menuBarStateEngine = new MenuBarStateEngine( this ) );
+        registerEngine( _menuStateEngine = new MenuStateEngine( this ) );
+        registerEngine( _toolBarStateEngine = new ToolBarStateEngine( this ) );
+
+    }
+
+    //____________________________________________________________________________________________
+    Animations::~Animations( void )
+    {
+        #if OXYGEN_DEBUG
+        std::cerr << "Oxygen::Animations::~Animations" << std::endl;
+        #endif
+
+        // delete all engines
+        for( BaseEngine::List::iterator iter = _engines.begin(); iter != _engines.end(); ++iter )
+        { delete *iter; }
+
+        // disconnect all signals from map
+        for( WidgetMap::iterator iter = _allWidgets.begin(); iter != _allWidgets.end(); iter++ )
+        { iter->second.disconnect(); }
+
+        // clear hooks
+        _sizeAllocationHook.disconnect();
+        _realizationHook.disconnect();
+        _innerShadowHook.disconnect();
+
+    }
+
+    //_________________________________________
+    void Animations::initialize( const QtSettings& settings )
+    {
+
+        const bool animationsEnabled( settings.animationsEnabled() );
+
+        // pass animations configuration to engines
+        widgetStateEngine().setApplicationName( settings.applicationName() );
+        widgetStateEngine().setEnabled( animationsEnabled && settings.genericAnimationsEnabled() );
+        widgetStateEngine().setDuration( settings.genericAnimationsDuration() );
+
+        arrowStateEngine().setApplicationName( settings.applicationName() );
+        arrowStateEngine().setEnabled( animationsEnabled && settings.genericAnimationsEnabled() );
+        arrowStateEngine().setDuration( settings.genericAnimationsDuration() );
+
+        scrollBarStateEngine().setApplicationName( settings.applicationName() );
+        scrollBarStateEngine().setEnabled( animationsEnabled && settings.genericAnimationsEnabled() );
+        scrollBarStateEngine().setDuration( settings.genericAnimationsDuration() );
+
+        tabWidgetStateEngine().setApplicationName( settings.applicationName() );
+        tabWidgetStateEngine().setEnabled( animationsEnabled && settings.genericAnimationsEnabled() );
+        tabWidgetStateEngine().setDuration( settings.genericAnimationsDuration() );
+
+        treeViewStateEngine().setApplicationName( settings.applicationName() );
+        treeViewStateEngine().setEnabled( animationsEnabled && settings.genericAnimationsEnabled() );
+        treeViewStateEngine().setDuration( settings.genericAnimationsDuration() );
+
+        menuBarStateEngine().setApplicationName( settings.applicationName() );
+        menuBarStateEngine().setAnimationsEnabled( animationsEnabled && (settings.menuBarAnimationType() != None) );
+        menuBarStateEngine().setFollowMouse( settings.menuBarAnimationType() == FollowMouse );
+        menuBarStateEngine().setDuration( settings.menuBarAnimationsDuration() );
+        menuBarStateEngine().setFollowMouseAnimationsDuration( settings.menuBarFollowMouseAnimationsDuration() );
+
+        menuStateEngine().setApplicationName( settings.applicationName() );
+        menuStateEngine().setEnabled( animationsEnabled && (settings.menuAnimationType() != None) );
+        menuStateEngine().setFollowMouse( settings.menuAnimationType() == FollowMouse );
+        menuStateEngine().setDuration( settings.menuAnimationsDuration() );
+        menuStateEngine().setFollowMouseAnimationsDuration( settings.menuFollowMouseAnimationsDuration() );
+
+        // for now, only Fade animations mode is supported for toolbar animations
+        toolBarStateEngine().setApplicationName( settings.applicationName() );
+        toolBarStateEngine().setEnabled( animationsEnabled && (settings.toolBarAnimationType() != None) );
+        toolBarStateEngine().setFollowMouse(  settings.toolBarAnimationType() == FollowMouse );
+        toolBarStateEngine().setDuration( settings.genericAnimationsDuration() );
+        toolBarStateEngine().setFollowMouseAnimationsDuration( settings.toolBarAnimationsDuration() );
+
+    }
+
+    //____________________________________________________________________________________________
+    void Animations::initializeHooks( void )
+    {
+        if( _hooksInitialized ) return;
+
+        // https://bugzilla.gnome.org/show_bug.cgi?id=643416
+        #if ENABLE_INNER_SHADOWS_HACK
+        if(!getenv("OXYGEN_DISABLE_INNER_SHADOWS_HACK"))
+            _innerShadowHook.connect( "realize", (GSignalEmissionHook)innerShadowHook, this );
+        #endif
+
+        _sizeAllocationHook.connect( "size-allocate", (GSignalEmissionHook)sizeAllocationHook, this );
+        _realizationHook.connect( "realize", (GSignalEmissionHook)realizationHook, this );
+
+        _hooksInitialized = true;
+    }
+
+    //____________________________________________________________________________________________
+    bool Animations::registerWidget( GtkWidget* widget )
+    {
+
+        if( _allWidgets.find( widget ) != _allWidgets.end() ) return false;
+
+        #if OXYGEN_DEBUG
+        std::cerr << "Oxygen::Animations::registerWidget - " << widget << " (" << (widget ? G_OBJECT_TYPE_NAME( widget ):"0x0") << ")" << std::endl;
+        #endif
+
+        Signal destroyId;
+        destroyId.connect( G_OBJECT( widget ), "destroy", G_CALLBACK( destroyNotifyEvent ), this );
+        _allWidgets.insert( std::make_pair( widget, destroyId ) );
+        return true;
+
+    }
+
+    //____________________________________________________________________________________________
+    void Animations::unregisterWidget( GtkWidget* widget )
+    {
+
+        #if OXYGEN_DEBUG
+        std::cerr << "Oxygen::Animations::unregisterWidget - " << widget << " (" << G_OBJECT_TYPE_NAME( widget ) << ")" << std::endl;
+        #endif
+
+        // find in map
+        WidgetMap::iterator iter( _allWidgets.find( widget ) );
+        assert( iter != _allWidgets.end() );
+
+        // disconnect signal
+        iter->second.disconnect();
+
+        // erase from map
+        _allWidgets.erase( widget );
+
+        // erase from all maps
+        for( BaseEngine::List::iterator iter = _engines.begin(); iter != _engines.end(); ++iter )
+        { (*iter)->unregisterWidget( widget ); }
+
+    }
+
+    //____________________________________________________________________________________________
+    void Animations::setEnabled( bool value )
+    {
+
+        for( BaseEngine::List::iterator iter = _engines.begin(); iter != _engines.end(); ++iter )
+        { (*iter)->setEnabled( value ); }
+
+    }
+
+    //____________________________________________________________________________________________
+    gboolean Animations::destroyNotifyEvent( GtkWidget* widget, gpointer data )
+    {
+        static_cast<Animations*>(data)->unregisterWidget( widget );
+        return FALSE;
+    }
+
+    //____________________________________________________________________________________________
+    gboolean Animations::sizeAllocationHook( GSignalInvocationHint*, guint, const GValue* params, gpointer data )
+    {
+
+        // get widget from params
+        GtkWidget* widget( GTK_WIDGET( g_value_get_object( params ) ) );
+
+        // check type
+        if( !GTK_IS_WIDGET( widget ) ) return FALSE;
+
+        // groupbox labels
+        #if ENABLE_GROUPBOX_HACK
+        if( static_cast<Animations*>( data )->groupBoxLabelEngine().contains( widget ) )
+        {
+            static_cast<Animations*>( data )->groupBoxLabelEngine().adjustSize( widget );
+            return TRUE;
+        }
+        #endif
+
+        #if ENABLE_COMBOBOX_LIST_RESIZE
+        // comboboxes
+        if( !GTK_IS_WINDOW( widget ) ) return TRUE;
+
+        GtkWindow* window( GTK_WINDOW( widget ) );
+        if( gtk_window_get_type_hint( window ) != GDK_WINDOW_TYPE_HINT_COMBO ) return TRUE;
+
+        Animations& animations( *static_cast<Animations*>(data) );
+        GtkWidget* combobox = animations.comboBoxEngine().find( widget );
+        if( !combobox ) combobox = animations.comboBoxEntryEngine().find( widget );
+        if( !combobox ) combobox = animations.comboEngine().find( widget );
+        if( !combobox ) return true;
+
+        int w, h;
+        gtk_window_get_size( window, &w, &h );
+
+        gint targetX, dummy, y;
+        gtk_window_get_position( window, &dummy, &y );
+        gdk_window_get_origin( gtk_widget_get_window( combobox ), &targetX, &dummy );
+
+        const GtkAllocation comboAllocation( Gtk::gtk_widget_get_allocation( combobox ) );
+        int uglyShadowWidth=!Gtk::gdk_default_screen_is_composited();
+        gtk_window_move( window, targetX + comboAllocation.x + 3 - uglyShadowWidth, y );
+
+        const GtkAllocation widgetAllocation( Gtk::gtk_widget_get_allocation( widget ) );
+        gtk_widget_set_size_request( widget, comboAllocation.width - 6 + 2*uglyShadowWidth, widgetAllocation.height );
+        #endif
+
+        return TRUE;
+
+    }
+
+    //____________________________________________________________________________________________
+    gboolean Animations::innerShadowHook( GSignalInvocationHint*, guint, const GValue* params, gpointer data )
+    {
+
+        #if GTK_CHECK_VERSION(2,24,2)
+
+        // get widget from params
+        GtkWidget* widget( GTK_WIDGET( g_value_get_object( params ) ) );
+
+        // check type
+        if( !GTK_IS_WIDGET( widget ) ) return FALSE;
+
+        // check enabled state
+        Animations& animations( *static_cast<Animations*>(data) );
+        if( !animations.innerShadowsEnabled() ) return TRUE;
+
+        if( Gtk::g_object_is_a( G_OBJECT( widget ), "SwtFixed" ) ) return TRUE;
+
+        GtkWidget* parent(gtk_widget_get_parent(widget));
+        if( !GTK_IS_SCROLLED_WINDOW( parent ) ) return TRUE;
+
+        GtkWidget* child(gtk_bin_get_child(GTK_BIN(parent)));
+        if(child!=widget) return TRUE;
+
+        #if OXYGEN_DEBUG
+        std::cerr
+            << "Oxygen::Animations::innerShadowHook -"
+            << " widget: " << widget << " (" << G_OBJECT_TYPE_NAME(widget) << ")"
+            << " parent: " << parent << " (" << G_OBJECT_TYPE_NAME(parent) << ")"
+            << " widget path: " << Gtk::gtk_widget_path( widget )
+            << " isTreeView: " << (GTK_IS_TREE_VIEW(widget)?"true":"false")
+            << " isTextView: " << (GTK_IS_TEXT_VIEW(widget)?"true":"false")
+            << std::endl;
+        #endif
+
+        animations.innerShadowEngine().registerWidget( parent );
+        animations.innerShadowEngine().registerChild( parent, widget );
+
+        #endif  // Gtk version
+        return TRUE;
+
+    }
+
+    //____________________________________________________________________________________________
+    gboolean Animations::realizationHook( GSignalInvocationHint*, guint, const GValue* params, gpointer data )
+    {
+
+        // get widget from params
+        GtkWidget* widget( GTK_WIDGET( g_value_get_object( params ) ) );
+
+        // check type
+        if( !GTK_IS_WIDGET( widget ) ) return FALSE;
+
+        if( GTK_IS_NOTEBOOK( widget ) )
+        { gtk_notebook_set_show_border( GTK_NOTEBOOK(widget), FALSE ); }
+
+        #if ENABLE_GROUPBOX_HACK
+        if( GTK_IS_LABEL( widget ) &&  GTK_IS_FRAME( gtk_widget_get_parent( widget ) ) )
+        {
+
+            GtkFrame *frame( GTK_FRAME( gtk_widget_get_parent( widget ) ) );
+            if( widget == gtk_frame_get_label_widget( frame ) && !Gtk::gtk_widget_find_parent( widget, "GtkPizza" ) )
+            {
+                #if OXYGEN_DEBUG
+                std::cout
+                    << "Oxygen::Animations::realizationHook -"
+                    << " widget: " << widget << " (" << G_OBJECT_TYPE_NAME( widget ) << ")"
+                    << " parent: " << frame << " (" << G_OBJECT_TYPE_NAME( frame ) << ")"
+                    << std::endl;
+                #endif
+
+                // modify alignment
+                gtk_frame_set_label_align( frame, 0.5, 0.0 );
+                gtk_frame_set_shadow_type( frame, GTK_SHADOW_OUT );
+
+                // register to engine
+                static_cast<Animations*>( data )->groupBoxLabelEngine().registerWidget( widget );
+                static_cast<Animations*>( data )->groupBoxLabelEngine().adjustSize( widget );
+
+            }
+
+        }
+        #endif
+
+        return TRUE;
+
+    }
+
+}
